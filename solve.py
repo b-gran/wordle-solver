d67e0978efe3125818bcdf5315a780276d9e3aa4
Bill Granfield <bill@billgranfield.com>
2022-01-15 21:03:34 -0500

    very slow first word solver

--- solve.py
+++ solve.py
@@ -3,6 +3,7 @@ import typing
 from enum import Enum
 import collections
 from dataclasses import dataclass
+from tqdm import tqdm
 
 top_5k_serialized = json.load(open('top_5k_words.json'))
 dictionary = [e['word'] for e in top_5k_serialized]
@@ -17,9 +18,6 @@ for w in dictionary:
         freq_by_char[c] = freq_by_char[c] + 1
 x = list(freq_by_char.items())
 x.sort(key=lambda i: i[1])
-
-
-# print(x)
 
 
 def char_freq(w: str) -> typing.Dict[str, int]:
@@ -47,7 +45,7 @@ WordClue = typing.List[LetterClue]
 
 # from https://github.com/lynn/hello-wordl/blob/00ef8569149bd998f5a992853172a1a30b4914b2/src/clue.ts#L12
 def get_clues(solution: str, guess: str) -> WordClue:
-    clue = []
+    clue = [0, 0, 0, 0, 0]
 
     elusive = collections.defaultdict(int)
     for i, c in enumerate(solution):
@@ -56,15 +54,15 @@ def get_clues(solution: str, guess: str) -> WordClue:
 
     for i, guess_char in enumerate(guess):
         if solution[i] == guess_char:
-            # clue[i] = LetterClue.PRESENT_CORRECT_LOCATION
-            clue.append(LetterClue(type=Clue.PRESENT_CORRECT_LOCATION, character=guess_char, position=i))
+            # clue.append(LetterClue(type=Clue.PRESENT_CORRECT_LOCATION, character=guess_char, position=i))
+            clue[i] = LetterClue(type=Clue.PRESENT_CORRECT_LOCATION, character=guess_char, position=i)
         elif elusive[guess_char] > 0:
             elusive[guess_char] -= 1
-            # clue[i] = LetterClue.PRESENT_INCORRECT_LOCATION
-            clue.append(LetterClue(type=Clue.PRESENT_INCORRECT_LOCATION, character=guess_char, position=i))
+            # clue.append(LetterClue(type=Clue.PRESENT_INCORRECT_LOCATION, character=guess_char, position=i))
+            clue[i] = LetterClue(type=Clue.PRESENT_INCORRECT_LOCATION, character=guess_char, position=i)
         else:
-            # clue[i] = LetterClue.NOT_PRESENT
-            clue.append(LetterClue(type=Clue.NOT_PRESENT, character=guess_char, position=i))
+            # clue.append(LetterClue(type=Clue.NOT_PRESENT, character=guess_char, position=i))
+            clue[i] = LetterClue(type=Clue.NOT_PRESENT, character=guess_char, position=i)
 
     return clue
 
@@ -149,6 +147,28 @@ def summarize_clues(clues: typing.List[WordClue]) -> typing.Dict[str, MetaClue]:
     return metaclues
 
 
+def filter_word(word: str, metaclues: typing.Dict[str, MetaClue]) -> bool:
+    counts = collections.defaultdict(int)
+    for i, char in enumerate(word):
+        mc = metaclues[char]
+        counts[char] += 1
+        if counts[char] > mc.upper_bound:
+            return False
+
+        if i in mc.impossible_positions:
+            return False
+
+    for mc in metaclues.values():
+        if counts[mc.character] < mc.lower_bound:
+            return False
+
+    return True
+
+
+def filter_impossible_words(dictionary: typing.List[str], metaclues: typing.Dict[str, MetaClue]) -> typing.List[str]:
+    return list(filter(lambda w: filter_word(w, metaclues), dictionary))
+
+
 def print_mc(mc: MetaClue):
     fp = "({})".format(''.join([str(d) if d in mc.fixed_positions else ' ' for d in range(5)]))
     ip = "({})".format(''.join([str(d) if d in mc.impossible_positions else ' ' for d in range(5)]))
@@ -160,20 +180,53 @@ def p(s):
     print('    fix     imp')
     for x in s.values():
         print(print_mc(x))
-        
 
-c1 = get_clues('clamp', 'conch')
-s1 = summarize_clues([c1])
-p(s1)
-print('=====')
-c2 = get_clues('clamp', 'occur')
-s2 = summarize_clues([c1, c2])
-p(s2)
-print('=====')
-c3 = get_clues('clamp', 'lathe')
-s3 = summarize_clues([c1, c2, c3])
-p(s3)
-print('=====')
-c4 = get_clues('clamp', 'lamps')
-s4 = summarize_clues([c1, c2, c3, c4])
-p(s4)
+
+# ## Example
+# c1 = get_clues('clamp', 'conch')
+# s1 = summarize_clues([c1])
+# print(len(filter_impossible_words(dictionary, s1)))
+# print(filter_impossible_words(dictionary, s1))
+# p(s1)
+# print('=====')
+# c2 = get_clues('clamp', 'occur')
+# s2 = summarize_clues([c1, c2])
+# print(len(filter_impossible_words(dictionary, s2)))
+# print(filter_impossible_words(dictionary, s2))
+# p(s2)
+# print('=====')
+# c3 = get_clues('clamp', 'lathe')
+# s3 = summarize_clues([c1, c2, c3])
+# print(len(filter_impossible_words(dictionary, s3)))
+# print(filter_impossible_words(dictionary, s3))
+# p(s3)
+# print('=====')
+# c4 = get_clues('clamp', 'lamps')
+# s4 = summarize_clues([c1, c2, c3, c4])
+# print(len(filter_impossible_words(dictionary, s4)))
+# print(filter_impossible_words(dictionary, s4))
+# p(s4)
+
+deltas = collections.defaultdict(int)
+
+# with tqdm(total=n * m) as pbar:
+#     for i1 in tqdm(range(n)):
+#         for i2 in tqdm(range(m)):
+#             # do something, e.g. sleep
+#             time.sleep(0.01)
+#             pbar.update(1)
+
+dict_length = len(dictionary)
+
+with tqdm(total=len(dictionary)**2) as pbar:
+    for solution in dictionary:
+        for guess in dictionary:
+            clues = get_clues(solution, guess)
+            sc = summarize_clues([clues])
+            delta = dict_length - len(filter_impossible_words(dictionary, sc))
+            deltas[guess] += delta
+            pbar.update(1)
+
+dictionary.sort(key=lambda w: -deltas[w])
+
+print('\n'.join(list(map(lambda w: '{} {}'.format(w, deltas[w]), dictionary[:100]))))
