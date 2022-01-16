321e2685dd588129a67230f33dc62d0d360408e0
Bill Granfield <bill@billgranfield.com>
2022-01-15 23:03:10 -0500

    basic BFS (much faster)

--- solve.py
+++ solve.py
@@ -169,7 +169,12 @@ def filter_word(word: str, metaclues: typing.Dict[str, MetaClue], required_lette
 
 
 def filter_impossible_words(dictionary: typing.List[str], metaclues: typing.Dict[str, MetaClue]) -> typing.List[str]:
-    return list(filter(lambda w: filter_word(w, metaclues), dictionary))
+    required_letters = set()
+    for mc in metaclues.values():
+        if mc.lower_bound > 0:
+            required_letters.add(mc.character)
+
+    return list(filter(lambda w: filter_word(w, metaclues, required_letters), dictionary))
 
 
 def count_impossible_words(dictionary: typing.List[str], metaclues: typing.Dict[str, MetaClue]) -> int:
@@ -179,10 +184,10 @@ def count_impossible_words(dictionary: typing.List[str], metaclues: typing.Dict[
             required_letters.add(mc.character)
 
     total = 0
-    # for w in dictionary:
-    #     # if filter_word(w, metaclues, required_letters):
-    #     #     total += 1
-    #     total += filter_word(w, metaclues, required_letters)
+    for w in dictionary:
+        # if filter_word(w, metaclues, required_letters):
+        #     total += 1
+        total += filter_word(w, metaclues, required_letters)
     return total
 
 
@@ -199,30 +204,139 @@ def p(s):
         print(print_mc(x))
 
 
+class Node:
+    character: str
+    children: typing.Dict[str, 'Node']
+    prefix_counts: typing.DefaultDict[str, int]
+    prefix: str
+
+    def __init__(self, character: str, parent_prefix_counts: typing.DefaultDict[str, int], parent_prefix: str):
+        self.character = character
+        self.children = dict()
+        self.prefix_counts = parent_prefix_counts.copy()
+        self.prefix_counts[character] += 1
+        self.prefix = parent_prefix + character
+
+
+def make_trie(dictionary: typing.List[str]) -> Node:
+    root = Node('', collections.defaultdict(int), '')
+
+    for word in dictionary:
+        curr = root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = Node(char, curr.prefix_counts, curr.prefix)
+            curr = curr.children[char]
+
+    return root
+
+
+dictionary_trie = make_trie(dictionary)
+
+
+# def filter_word(word: str, metaclues: typing.Dict[str, MetaClue], required_letters: typing.Set[str]) -> int:
+#     counts = collections.defaultdict(int)
+#
+#     # for i, char in enumerate(word):
+#     for i in range(5):
+#         char = word[i]
+#         mc = metaclues[char]
+#         counts[char] += 1
+#         if counts[char] > mc.upper_bound:
+#             return 0
+#
+#         if i in mc.impossible_positions:
+#             return 0
+#
+#     for required_letter in required_letters:
+#         if counts[required_letter] < metaclues[required_letter].lower_bound:
+#             return 0
+#
+#     return 1
+
+
+def filter_dict_trie(dict_root: Node, metaclues: typing.Dict[str, MetaClue]) -> int:
+    required_letters = set()
+    for mc in metaclues.values():
+        if mc.lower_bound > 0:
+            required_letters.add(mc.character)
+
+    depth = 0
+    frontier = [dict_root]
+    while frontier and depth < 5:
+        next_level = []
+
+        # next_level = [
+        #     n.children[char] for n in frontier for char in n.children if
+        #     n.prefix_counts[char] <= metaclues[char].upper_bound and depth not in metaclues[char].impossible_positions
+        # ]
+
+        for n in frontier:
+            for char in n.children:
+                mc = metaclues[char]
+                if n.prefix_counts[char] > mc.upper_bound:
+                    continue
+                if depth in mc.impossible_positions:
+                    continue
+                next_level.append(n.children[char])
+
+        depth += 1
+        frontier = next_level
+
+    num_valid = 0
+    for n in frontier:
+        valid = True
+        for required_letter in required_letters:
+            if n.prefix_counts[required_letter] < metaclues[required_letter].lower_bound:
+                valid = False
+                break
+        if valid:
+            num_valid += 1
+
+    # print(len(frontier))
+    # print(len([n for n in frontier if n))
+    # print([n.prefix for n in frontier])
+    # print('frontier size', num_valid)
+
+    return num_valid
+
+
 # ## Example
 # c1 = get_clues('clamp', 'conch')
 # s1 = summarize_clues([c1])
 # print(len(filter_impossible_words(dictionary, s1)))
 # print(filter_impossible_words(dictionary, s1))
 # p(s1)
+# print(filter_dict_trie(dictionary_trie, s1))
 # print('=====')
 # c2 = get_clues('clamp', 'occur')
 # s2 = summarize_clues([c1, c2])
 # print(len(filter_impossible_words(dictionary, s2)))
 # print(filter_impossible_words(dictionary, s2))
 # p(s2)
+# print(filter_dict_trie(dictionary_trie, s2))
 # print('=====')
 # c3 = get_clues('clamp', 'lathe')
 # s3 = summarize_clues([c1, c2, c3])
 # print(len(filter_impossible_words(dictionary, s3)))
 # print(filter_impossible_words(dictionary, s3))
 # p(s3)
+# print(filter_dict_trie(dictionary_trie, s3))
 # print('=====')
 # c4 = get_clues('clamp', 'lamps')
 # s4 = summarize_clues([c1, c2, c3, c4])
 # print(len(filter_impossible_words(dictionary, s4)))
 # print(filter_impossible_words(dictionary, s4))
 # p(s4)
+# print(filter_dict_trie(dictionary_trie, s4))
+
+# ## Example 2
+# c1 = get_clues('which', 'twice')
+# s1 = summarize_clues([c1])
+# print(len(filter_impossible_words(dictionary, s1)))
+# print(filter_impossible_words(dictionary, s1))
+# p(s1)
+# filter_dict_trie(dictionary_trie, s1)
 
 deltas = collections.defaultdict(int)
 
@@ -238,26 +352,29 @@ dict_length = len(dictionary)
 
 def process():
     it = 0
-    # with tqdm(total=len(dictionary)**2) as pbar:
-    with tqdm(total=1000) as pbar:
+    with tqdm(total=len(dictionary)**2) as pbar:
+    # with tqdm(total=1000) as pbar:
         for solution in dictionary:
             for guess in dictionary:
                 clues = get_clues(solution, guess)
                 sc = summarize_clues([clues])
 
                 # delta = dict_length - len(filter_impossible_words(dictionary, sc))
-                delta = dict_length - count_impossible_words(dictionary, sc)
+                # delta = dict_length - count_impossible_words(dictionary, sc)
+
+                delta = dict_length - filter_dict_trie(dictionary_trie, sc)
+
                 deltas[guess] += delta
                 pbar.update(1)
-                it += 1
 
-                if it >= 1000:
-                    return
+                # it += 1
+                # if it >= 1000:
+                #     return
 
 
 process()
 
-dictionary.sort(key=lambda w: -deltas[w])
-
-print('\n'.join(list(map(lambda w: '{} {}'.format(w, deltas[w]), dictionary[:100]))))
-print('\n'.join(list(map(lambda w: '{} {}'.format(w, deltas[w]), dictionary[-100:]))))
+# dictionary.sort(key=lambda w: -deltas[w])
+#
+# print('\n'.join(list(map(lambda w: '{} {}'.format(w, deltas[w]), dictionary[:100]))))
+# print('\n'.join(list(map(lambda w: '{} {}'.format(w, deltas[w]), dictionary[-100:]))))
